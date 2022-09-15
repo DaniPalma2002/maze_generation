6d1fb82d4ef84ded7619e0cdab423dbb8140e752
Daniel Pereira <72202548+DaniPalma2002@users.noreply.github.com>
2022-09-15 13:50:41 +0100

    added start and end

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@
 // Growing Tree algorithm
 
 var CELL_SIZE = 20; // px
-var ROWS = 5;
-var COLS  = 5;
+var ROWS = 10;
+var COLS  = 10;
 var grid = []; // matrix
 var queue = []; // backtracking queue
 var current; // current cell being visited
@@ -14,8 +14,9 @@ var bias_list = ['N', 'R', 'O', 'M', 'N/R(75/25)'];
     // O - Oldest
     // M - Middle
     // N/R(75/25) Newest/Random, 75/25 split
+var bias = bias_list[0]
 
-var theme = [255, 10, 4] // background, stroke color, stroke weight
+var theme = [255, 10, 2] // background, stroke color, stroke weight
 
 
 document.getElementById("submit").onclick = function() {
@@ -39,7 +40,7 @@ function setup() {
     //console.log(row_param == null)
     if (!(row_param == null)) ROWS = int(row_param)
     if (!(col_param == null)) COLS = int(col_param)
-    console.log(ROWS, COLS)
+    //console.log(ROWS, COLS)
 
     createCanvas(COLS*CELL_SIZE, ROWS*CELL_SIZE);
 
@@ -51,7 +52,7 @@ function setup() {
     }
 
     // choose first node
-    current = grid[get_random_int(0, grid.length)].grid_place
+    current = grid[floor(random(grid.length))].grid_place
     queue.push(current)
     grid[current].state = 1
 
@@ -64,7 +65,10 @@ function draw() {
         grid[i].show();
     }
     
-    if (queue.length === 0) noLoop()
+    if (queue.length === 0) {
+        noLoop()
+        choose_start_and_end()
+    }
     carve_path();
     
     
@@ -79,7 +83,7 @@ function Node(i, j) {
     this.state = 0 // if is visited
     this.wall = [1,1,1,1] // top, right, bottom, left
     this.grid_place = this.j * COLS + this.i //index in grid
-
+    this.number_of_neighbours = 0
 
     this.show = function() {
         var x = this.i * CELL_SIZE;
@@ -108,31 +112,41 @@ function Node(i, j) {
         n_row = n_row.filter(item => (grid[item].j == this.j && grid[item].state == 0))
    
         a = n_col.concat(n_row)
-        
-        return a
+        this.number_of_neighbours = a.length
+        // no unvisited neighbours
+        if (a.length === 0) return -1
+
+        rand = floor(random(0, a.length))
+        return a[rand]
+    }
+
+    this.number_of_walls = function() {
+        var count = 0
+        var arr = this.wall
+        arr.forEach(i => (i === 1 && count++))
+        return count
     }
 
 }
 
-function choose_element_of_queue(bias) {
+function choose_element_of_queue() {
     if (bias == 'N') return (queue.length - 1)
-    else if (bias == 'R') return get_random_int(0, queue.length - 1)
+    else if (bias == 'R') return floor(random(queue.length))
     else if (bias == 'O') return 0
     else if (bias == 'M') return floor((queue.length-1)/2)
     else if (bias == 'N/R(75/25)') {
-        rnd = get_random_int(1, 5)
-        if (rnd == 1) return get_random_int(0, queue.length - 1)
+        rnd = floor(random(1, 5))
+        if (rnd == 1) return floor(random(queue.length))
         else return (queue.length - 1)
     }
 }
 
 // return int random between min (inclusive) and max (exclusive)
-function get_random_int(min, max) {
+function get_random_int(min, max) { 
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
 
 function shuffle_array(array) {
     let currentIndex = array.length,  randomIndex;
@@ -155,17 +169,16 @@ function shuffle_array(array) {
 
 function carve_path() {
     
-    var index = choose_element_of_queue(bias_list[0]) // index of queue
+    var index = choose_element_of_queue(bias) // index of queue
     
     if (queue.length == 0) return
     current = queue[index]
     //console.log(current)
     n = grid[current].neighbours()
     
-    if (!(n.length === 0)) {
-        var rand = get_random_int(0, n.length - 1);
+    if (!(n == -1)) {
         
-        next_node = n[rand];
+        next_node = n;
         
         grid[next_node].state = 1
         var curr_wall, next_wall;
@@ -179,6 +192,9 @@ function carve_path() {
     }
     else {
         // no unvisited neighbours, remove from queue
+        if (bias == bias_list[0]) {
+            queue.pop()
+        } 
         queue.splice(index, 1)
     }
 }
@@ -234,3 +250,36 @@ function URL_add_parameter(url, param, value){
     return parser.href;
 }
 
+function choose_start_and_end() {
+    let start = 0
+    let end = grid.length-1
+    //let n_flag = false
+    fill(0, 255, 0)
+    noStroke()
+    ellipse(CELL_SIZE/2, CELL_SIZE/2, CELL_SIZE/2)
+
+    // find a dead end
+    for (var k=0; k < grid.length; k++) {
+        console.log(grid[k].number_of_walls(), k)
+        if (grid[k].i >= floor(COLS/2) && grid[k].j >= floor(ROWS/2)) {
+            //console.log('b')
+            if (grid[k].number_of_walls() == 3) {
+                console.log('c', k)
+                fill(255, 0, 0)
+                let end_i = grid[k].i * CELL_SIZE
+                let end_j = grid[k].j * CELL_SIZE
+                ellipse(end_i + CELL_SIZE/2, end_j + CELL_SIZE/2, CELL_SIZE/2)
+                //n_flag = true
+                return
+            }
+        }
+    }
+    //if (!n_flag){
+    let end_i = grid[end].i * CELL_SIZE
+    let end_j = grid[end].j * CELL_SIZE
+
+    fill(255, 0, 0)
+    console.log('a')
+    ellipse(end_i + CELL_SIZE/2, end_j + CELL_SIZE/2, CELL_SIZE/2)
+    //} 
+}
