49d2a45b7647e280d2c48335953eaadd254aa9ac
Daniel Pereira <danielppereira2002@tecnico.ulisboa.pt>
2023-04-03 12:34:32 +0100

    prettier support

--- support.js
+++ support.js
@@ -1,18 +1,20 @@
 function user_input() {
-    //createCanvas(100, 100)
-    background(255)
+	//createCanvas(100, 100)
+	background(255);
 
-    main_text = createDiv("insert data")
-    main_text.id('main_text');
-    main_text.position(10, 10);
+	main_text = createDiv("insert data");
+	main_text.id("main_text");
+	main_text.position(10, 10);
 
-    start_button = createButton('START');
-    start_button.mouseReleased(start_test);
-    start_button.position(width/2 - start_button.size().width/2, height/2 - start_button.size().height/2);
-
+	start_button = createButton("START");
+	start_button.mouseReleased(start_test);
+	start_button.position(
+		width / 2 - start_button.size().width / 2,
+		height / 2 - start_button.size().height / 2
+	);
 }
 
 function start_test() {
-    main_text.remove();
-    start_button.remove();  
+	main_text.remove();
+	start_button.remove();
 }
